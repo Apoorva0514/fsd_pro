26e98483058b4eb533a588d053d3a8558152b620
Apoorva0514 <91599384+Apoorva0514@users.noreply.github.com>
2024-03-01 10:18:32 +0530

    today

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+
+console.log(process.env.port) // remove this after you've confirmed it is working
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(process.env.port, ()=>{
+    console.log("Listening port...",process.env.port)
+})
